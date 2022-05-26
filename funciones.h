241faa4137bb7cb8d84b06522a01dfda66a06500
Ruyllex <ruymori630@gmail.com>
2022-05-26 16:33:59 -0300

    xd

--- funciones.h
+++ funciones.h
@@ -3,26 +3,26 @@
 //Pre:-
 //Post: Muestra el menu del programa
 
-void mostrar_menu();
+void mostrar_menu();//listo
 
-void quitar_lectura_de_una_lista(Lista<Escritor> * lista_de_escritores);
+void quitar_lectura_de_una_lista(Lista<Escritor> * lista_de_escritores);//listo
 
 
-void crear_lista_escritores(Lista<Escritor> * lista_de_escritores);
+void crear_lista_escritores(Lista<Escritor> * lista_de_escritores);//listo
 
-void listar_escritores(Lista<Escritor> * lista_escritores);
+void listar_escritores(Lista<Escritor> * lista_escritores);//listo
 
-void agregar_escritor(Lista<Escritor> *Lista_de_escritores);
+void agregar_escritor(Lista<Escritor> *Lista_de_escritores);//listo
 
-void mostrar_nombre_y_fallecimiento_escritor(Lista<Escritor>* lista_de_escritores);
+void mostrar_nombre_y_fallecimiento_escritor(Lista<Escritor>* lista_de_escritores);//listo
 
-void modificar_ano_fallecimiento_escritor(Lista<Escritor> * lista_de_escritores);
+void modificar_ano_fallecimiento_escritor(Lista<Escritor> * lista_de_escritores);//listo
 
-//Lectura sortear_una_lectura(Lista<Escritor> * lista_de_escritores);
+//Lectura sortear_una_lectura(Lista<Escritor> * lista_de_escritores);//falta
 
-void listar_novelas_por_genero(Lista<Escritor> * lista_de_escritores);
+void listar_novelas_por_genero(Lista<Escritor> * lista_de_escritores);//falta
 
-void listar_lecturas(Lista<Escritor> * lista_de_escritores);
+void listar_lecturas(Lista<Escritor> * lista_de_escritores);//falta
 
-void cambiar_dato_escritor(Lista<Escritor> * lista_de_escritores);
+void cambiar_dato_escritor(Lista<Escritor> * lista_de_escritores);//falta
 
